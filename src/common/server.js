a7e8779692c5f6e9cfa3d0fb6c0e562fdd7e5bdd
ROR2022 <rami.ror279@gmail.com>
2023-03-22 10:01:56 -0600

    revisando cors

--- src/common/server.js
+++ src/common/server.js
@@ -12,7 +12,7 @@ const app = new Express();
 export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`)
-    
+    //revisando el cors
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }))
     app.use(cors())
     app.use(
